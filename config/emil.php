97bfd603059b8c4b1f27f4b8e0296741868c6425
Frédéric Blanc <frederic.blanc@itanea.fr>
2024-03-28 19:00:20 +0100

    Revert "Add theme section in Emil config file"
    This reverts commit 62cf3b1c848e7945653165dbc5c3987559119451.

--- config/emil.php
+++ config/emil.php
@@ -1,16 +1,6 @@
 <?php
 
+// config for Happytodev/Emil
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Themes
-    |--------------------------------------------------------------------------
-    |
-    | Emil is themable. You can define here the thema name you want to use. By
-    | defaut Emil comes with four themes: Porto, Minimal Blog, Pinwheel and
-    | Atom. Of course, you are free to build your own theme.
-    |
-    */
-    'theme' => env('EMIL_THEME', 'pinwheel'),
 ];
